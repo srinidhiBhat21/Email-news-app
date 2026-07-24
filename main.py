921e09c373951bfb126fede66afe298f12b12444
SrinidhiBhat21 <srinidhi4bhat@gmail.com>
2026-07-24 13:43:12 +0530

    Access the news titles and description

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import requests
 
-url = "https://finance.yahoo.com"
+api_key ="38b62e3fc7b147fa948b46277ce027e738b62e3fc7b147fa948b46277ce027e7"
+url = "https://newsapi.org/v2/everything?q=tesla&from=2026-06-24&sortBy=publishedAt&apiKey=38b62e3fc7b147fa948b46277ce027e7"
 
+#Make Request
 request = requests.get(url)
-content = request.text
-print(content)
+
+#Get a dict
+content = request.json()
+
+for article in content['articles']:
+    print(article['title'])
+    print(article['description'])
